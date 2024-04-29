cd955f8725e3a8e3e3cb117db99556886abec800
Prakash Gurung <prakashgrg4400@gmail.com>
2024-04-29 23:08:44 +0545

    odd and even button UI added

--- js/index.js
+++ js/index.js
@@ -1,16 +1,14 @@
+let input = document.getElementById("result");
+
 function inputData(num) {
-  let input = document.getElementById("result");
   input.value += num;
 }
 
 function handleOutput() {
-  let input = document.getElementById("result");
-
   total = eval(input.value);
   input.value = total;
 }
 
 function handleClear() {
-  let input = document.getElementById("result");
   input.value = "";
 }
